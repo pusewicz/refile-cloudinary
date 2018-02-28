e8ba641e2b2b4d3f3f25e9222cf3a6150edc2094
Cecatto <bruno.cecatto@codeminer42.com>
2018-02-28 11:21:59 -0300

    fix from comments in PR

--- spec/refile/cloudinary_spec.rb
+++ spec/refile/cloudinary_spec.rb
@@ -11,15 +11,18 @@ RSpec.describe Refile::Cloudinary do
   it "has a version number" do
     expect(Refile::Cloudinary::VERSION).not_to be nil
   end
+  
   describe "behavior Refile::Cloudinary" do
-    let(:backend_default) { Refile::Cloudinary.new(**config) }
-    let(:backend_max_size_100) { Refile::Cloudinary.new({ **config }, max_size: 100) }
+    let(:backend_default) { Refile::Cloudinary.new(config) }
+    let(:backend_max_size_100) { Refile::Cloudinary.new({ **config }, max_size: 100) }  
     it "expect backend max size equal 10_485_760" do
       expect(backend_default.max_size).to eq 10_485_760
     end
+
     it "object not nil" do
       expect(backend_default).not_to be_nil 
     end
+
     it "change to 100 max_size" do
       expect(backend_max_size_100.max_size).to eq 100
     end
