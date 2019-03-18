5531f92755265a1069bac2af76c50430ec5f0dac
Piotr Usewicz <piotr@layer22.com>
2019-03-18 15:01:45 +0100

    Make rubocop happier

--- spec/refile/cloudinary_spec.rb
+++ spec/refile/cloudinary_spec.rb
@@ -1,7 +1,7 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Refile::Cloudinary do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Refile::Cloudinary::VERSION).not_to be nil
   end
 end
